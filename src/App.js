c1c63f4f92a10dc7b36c7692cceedfcb7d869e6c
Diego <Diego.iribe@outlook.es>
2024-04-06 17:59:05 -0700

    arreglo de bug en spa

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import ContactPage from './Pages/Contact'
 import Shop from './Pages/Shop'
 import Footer from './Components/Footer'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 function App() {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+      window.scrollTo(0, 0)
+    }, [pathname])
+
+    return null
+  }
+
   return (
     <Router className="App">
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
